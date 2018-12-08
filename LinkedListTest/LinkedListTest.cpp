63010d57e6e6eb05afc5f83ab1fae8e1571721ec
Michel Isorinne <45288006+Isorinne@users.noreply.github.com>
2018-12-08 22:12:45 +0100

    La till en test fitxure i LinkedListTest

--- LinkedListTest/LinkedListTest.cpp
+++ LinkedListTest/LinkedListTest.cpp
@@ -1,42 +1,52 @@
 #include "pch.h"
 #include "../cpp-templates-solution/LinkedList.h"
 
+class LinkedListIntTest: public :: testing::Test {
+public:
+	LinkedListIntTest() {}
+	void SetUp() {}
+	void TearDown() {}
+	~LinkedListIntTest() {}
 
-TEST(LinkedListIntTest, When_Inserting_One_Value_To_List_Data_Is_Valid) {
 	LinkedList<int> intList;
-	int result = 10;
+	int result;
+
+	void insertInts(int n) {
+		for (int i = 0; i < n; i++)
+			intList.insert(i, i);
+	}
+};
+
+
+TEST_F(LinkedListIntTest, When_Inserting_One_Value_To_List_Data_Is_Valid) {
+	
+	result = 10;
 
 	intList.insert(0, 10);
 
 	EXPECT_EQ(intList.get_data(0), result);
 }
 
-TEST(LinkedListIntTest, When_Inserting_100_Values_To_List_Data_Is_Valid) {
-	LinkedList<int> intList;
-	int result = 42;
+TEST_F(LinkedListIntTest, When_Inserting_100_Values_To_List_Data_Is_Valid) {
+	result = 42;
 
-	for (int i = 0; i < 100; i++)
-		intList.insert(i, i);
+	insertInts(100);
 
 	EXPECT_EQ(intList.get_data(42), result);
 }
 
-TEST(LinkedListIntTest, When_Inserting_Size_Follows) {
-	LinkedList<int> intList;
-	int result = 50;
+TEST_F(LinkedListIntTest, When_Inserting_Size_Follows) {
+	result = 50;
 
-	for (int i = 0; i < 50; i++)
-		intList.insert(i, i);
+	insertInts(50);
 
 	EXPECT_EQ(intList.get_size(), result);
 }
 
-TEST(LinkedListIntTest, When_Inserting_And_Popping_Size_Follows) {
-	LinkedList<int> intList;
-	int result = 5;
+TEST_F(LinkedListIntTest, When_Inserting_And_Popping_Size_Follows) {
+	result = 5;
 
-	for (int i = 0; i < 50; i++)
-		intList.insert(i, i);
+	insertInts(50);
 
 	for (int i = 0; i < 45; i++)
 		intList.pop(0);
@@ -44,9 +54,8 @@ TEST(LinkedListIntTest, When_Inserting_And_Popping_Size_Follows) {
 	EXPECT_EQ(intList.get_size(), result);
 }
 
-TEST(LinkedListIntTest, When_Popping_Last_In_List_Size_Is_Zero) {
-	LinkedList<int> intList;
-	int result = 0;
+TEST_F(LinkedListIntTest, When_Popping_Last_In_List_Size_Is_Zero) {
+	result = 0;
 
 	intList.insert(0, 555);
 	intList.pop(0);
@@ -54,16 +63,12 @@ TEST(LinkedListIntTest, When_Popping_Last_In_List_Size_Is_Zero) {
 	EXPECT_EQ(intList.get_size(), result);
 }
 
-TEST(LinkedListIntTest, When_Popping_Last_In_List_Insert_Still_Works_And_Data_Is_Valid) {
-	LinkedList<int> intList;
-	int result = 42;
+TEST_F(LinkedListIntTest, When_Popping_Last_In_List_Insert_Still_Works_And_Data_Is_Valid) {
+	result = 3;
 
-	intList.insert(0, 555);
+	insertInts(1);
 	intList.pop(0);
-	intList.insert(0, 23);
-	intList.insert(1, 17);
-	intList.insert(2, 5);
-	intList.insert(3, 42);
+	insertInts(4);
 
 	EXPECT_EQ(intList.get_data(3), result);
 }
