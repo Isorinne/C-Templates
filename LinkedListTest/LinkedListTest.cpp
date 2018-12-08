e92ecd48e97adce35b6d500ebd81dcde2a6390b4
Michel Isorinne <45288006+Isorinne@users.noreply.github.com>
2018-12-08 01:09:09 +0100

    Småfix i LinkedList, testade göra några tester på den också.

--- LinkedListTest/LinkedListTest.cpp
+++ LinkedListTest/LinkedListTest.cpp
@@ -1,7 +1,49 @@
 #include "pch.h"
-//#include "../cpp-templates-solution/LinkedList.h"
+#include "../cpp-templates-solution/LinkedList.h"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(LinkedListTest, Inserting_1_Int_To_List) {
+	LinkedList<int> intList;
+	int result = 10;
+
+	intList.insert(0, 10);
+
+	EXPECT_EQ(intList.get_data(0), result);
+}
+
+TEST(LinkedListTest, Inserting_100_Ints_To_List) {
+	LinkedList<int> intList;
+	int result = 99;
+
+	for (int i = 0; i < 100; i++)
+		intList.insert(i, i);
+
+	EXPECT_EQ(intList.get_data(99), result);
+}
+
+TEST(LinkedListTest, When_Inserting_Size_Follows) {
+	LinkedList<int> intList;
+	int result = 50;
+
+	for (int i = 0; i < 50; i++)
+		intList.insert(i, i);
+
+	EXPECT_EQ(intList.get_size(), result);
+}
+
+TEST(LinkedListTest, When_Inserting_And_Popping_Size_Follows) {
+	LinkedList<int> intList;
+	int result = 5;
+
+	for (int i = 0; i < 50; i++)
+		intList.insert(i, i);
+
+	for (int i = 0; i < 45; i++)
+		intList.pop(0);
+
+	EXPECT_EQ(intList.get_size(), result);
 }
--- cpp-templates-solution/LinkedList.h
+++ cpp-templates-solution/LinkedList.h
@@ -19,7 +19,7 @@ public:
 		size = 0;
 		head = nullptr;
 	}
-	 
+
 	~LinkedList<T>() {
 		Node * current_node = nullptr;
 		while (head->next != nullptr) {
@@ -32,7 +32,7 @@ public:
 
 
 	T get_data(int index) {
-		if (head == nullptr or size < index + 1) return 0;
+		if (head == nullptr || size < index + 1) return 0;
 		if (index == 0) return head->data;
 		Node * previous_node = new Node;
 		Node * current_node = head;
@@ -42,14 +42,14 @@ public:
 		}
 		return current_node->data;
 	}
-	
+
 	int get_size() {
 		return size;
 	}
 
 	void insert(int index, T data) {
-		if (size < index + 1) return;
-		if (head == nullptr or index == 0) {
+
+		if (head == nullptr || index == 0) {
 			Node * new_head = new Node;
 			new_head->data = data;
 			new_head->next = head;
@@ -80,11 +80,15 @@ public:
 		new_node->next = current_node;
 		size++;
 	}
-	 
+
 	void pop(int index) {
 		if (head == nullptr) return;
 		if (size < index + 1) return;
-		if (head == nullptr or index == 0) {
+		if (head->next == nullptr || index == 0) {
+			if (size == 1) {
+				size--;
+				return;
+			}
 			Node * second_last_node = head;
 			while (second_last_node->next->next != nullptr)
 				second_last_node = second_last_node->next;
@@ -97,7 +101,7 @@ public:
 		if (size == index + 1) {
 			head = head->next;
 			delete(node_to_pop);
-			size--; 
+			size--;
 			return;
 		}
 		Node * previous_node = new Node;
